152d56137977164d4338230f0f79e763338ab6d1
AnnabelleHermey <annabelle.hermey@gmail.com>
2025-04-16 12:30:00 -0400

    Update to be my project page

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  // Open modal when project is clicked
+  $('.project').click(function () {
+      const modalId = $(this).data('modal');
+      $('#' + modalId).fadeIn();
+  });
+
+  // Close modal when close button is clicked
+  $('.close-btn').click(function () {
+      const modalId = $(this).data('modal');
+      $('#' + modalId).fadeOut();
+  });
+
+  // Close modal when clicked outside of modal content
+  $(window).click(function (event) {
+      $('.modal').each(function () {
+          if ($(event.target).is(this)) {
+              $(this).fadeOut();
+          }
+      });
+  });
+});
